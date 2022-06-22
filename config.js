51746b3af3ae6dd47e74e39f4c39a53d57fc05ac
tellmemyworth <tellmemyworth@gmail.com>
2022-06-21 19:10:12 -0500

    Tokens added

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = require('./config.json')
 
 function configSetting() {
     // console.log(config.database_dev)
-    return config.database_dev
+    return config.database_prod
 }
 
 configSetting()
